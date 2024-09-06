24ba397cc69132676300ea15971bd245572e577c
sardor01 <sardor.astanov.01@mail.ru>
2024-09-07 00:45:28 +0500

    feat: init

---
+++ README.md
@@ -0,0 +1,13 @@
+# @sarast/prettier-config [![npm](https://img.shields.io/npm/v/@sarast/prettier-config.svg)](https://npmjs.com/package/@sarast/prettier-config)
+
+Prettier config.
+
+## Install
+
+```bash
+pnpm add -D @sarast/prettier-config
+```
+
+## License
+
+[MIT](./LICENSE) License © 2024-PRESENT [Sardor Astanov](https://github.com/sardor01)
---
+++ index.js
@@ -0,0 +1,48 @@
+// @ts-check
+
+/** @type {import('prettier').Config} */
+module.exports = {
+  bracketSpacing: true,
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  endOfLine: 'lf',
+  overrides: [
+    {
+      files: [
+        '**/node_modules/**',
+        '**/dist/**',
+        '**/output/**',
+        '**/coverage/**',
+        '**/temp/**',
+        '**/.vitepress/cache/**',
+        '**/.nuxt/**',
+        '**/.vercel/**',
+        '**/.changeset/**',
+        '**/.idea/**',
+        '**/.output/**',
+        '**/.vite-inspect/**',
+
+        '**/CHANGELOG*.md',
+        '**/*.min.*',
+        '**/LICENSE*',
+        '**/__snapshots__',
+        '**/auto-import?(s).d.ts',
+        '**/components.d.ts',
+        '**/typed-router.d.ts',
+        '**/pnpm-lock.yaml',
+      ],
+      options: {
+        requirePragma: true,
+      },
+    },
+    {
+      files: '*.svg',
+      options: {
+        parser: 'html',
+      },
+    },
+  ],
+};
